ec2d678222ffa8ee2ca8bd030722b82f9c1a50a5
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-01-27 21:14:23 -0200

    no message

---
+++ passagem por copia e referencia dados compostos/Model/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Model
+{
+    class Point
+    {
+        public double x{get; set;}
+        public double y{ get; set;}
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public string toString()
+        { 
+            return "("+this.x+","+this.y+")";
+        }
+    }
+
+    struct Vector
+    {
+        public double x ;
+        public double y;
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public string toString()
+        {
+            return "(" + this.x + "," + this.y + ")";
+        }
+    }
+}
---
+++ passagem por copia e referencia dados compostos/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(11, 11);
+            Console.WriteLine(p.toString());
+            soma1(p);
+            Console.WriteLine(p.toString());
+
+            Vector v = new Vector(13, 13);
+            Console.WriteLine(v.toString());
+            soma1(v);
+            Console.WriteLine(v.toString());
+
+
+
+            Console.ReadLine();
+        }
+
+        static void soma1(Point point)
+        {
+            point.x++;
+            point.y++;
+        }
+
+          static void soma1(Vector vector)
+        {
+            vector.x++;
+            vector.y++;
+        }
+
+
+
+    }
+}
