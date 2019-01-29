9f8714b046e844316cee091000445960b292ae8a
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-01-28 23:20:14 -0200

    no message

---
+++ Padrões de Projeto/Singleton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Padrões_de_Projeto
+{
+    /* a classe é selada para nao ser possivel fazer herança*/
+   public sealed class Singleton
+    {
+        private static readonly Singleton instance;
+        private int numeroDeInstancias = 0;
+        private Singleton()=> this.numeroDeInstancias++;
+    
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+}
+
+
+
+
+//namespace ConsoleApp2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            Singleton s = Singleton.Instance;
+
+
+//            Console.ReadLine();
+//        }
+
+
+
+//    }
+}
